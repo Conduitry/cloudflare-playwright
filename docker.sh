2badb4fe0bf087b310faa5eb6681a32d0e7944cf
Conduitry <git@chor.date>
2022-06-08 04:08:53 -0400

    mount as read-only in Docker container

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm --volume "$PWD":/app mcr.microsoft.com/playwright:v1.22.2 node /app $1
+docker run --rm --volume "$PWD":/app:ro mcr.microsoft.com/playwright:v1.22.2 node /app $1
