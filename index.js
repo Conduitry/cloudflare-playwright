85040ea3b88b80e37e45108d9f9a4b94b5c1b5a8
Conduitry <git@chor.date>
2022-06-07 21:41:51 -0400

    wait for cookie rather than hardcoding wait time

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import { firefox } from 'playwright';
 const browser = await firefox.launch();
 const context = await browser.newContext();
 const page = await context.newPage();
+const user_agent = await page.evaluate('navigator.userAgent');
 
 await page.goto(`https://${process.argv[2]}`);
-await new Promise(res => setTimeout(res, 5000));
 
-const cf_clearance = (await context.cookies()).find(({ name }) => name === 'cf_clearance').value;
-const user_agent = await page.evaluate('navigator.userAgent');
-console.log(JSON.stringify({ headers: { cookie: `cf_clearance=${cf_clearance}`, 'user-agent': user_agent } }));
+let cf_clearance;
+while (!cf_clearance) {
+	await new Promise(res => setTimeout(res, 100));
+	cf_clearance = (await context.cookies()).find(({ name }) => name === 'cf_clearance');
+}
+console.log(JSON.stringify({ headers: { cookie: `cf_clearance=${cf_clearance.value}`, 'user-agent': user_agent } }));
 
 await browser.close();
