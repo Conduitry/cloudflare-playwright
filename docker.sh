8bb45d1eff6758329c3d8afddf5c529d06be26fe
Conduitry <git@chor.date>
2022-06-20 03:11:20 -0400

    allow running docker.sh from any directory

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm --volume "$PWD":/app:ro mcr.microsoft.com/playwright:v1.22.2 node /app $1
+docker run --rm --volume "$(dirname "$(readlink --canonicalize "$0")")":/app:ro mcr.microsoft.com/playwright:v1.22.2 node /app $1
