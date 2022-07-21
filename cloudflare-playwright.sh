d3325eccb4cac315bc671508334def7e0fdc8233
Conduitry <git@chor.date>
2022-07-21 10:18:16 -0400

    support earlier Playwright versions
    Version 1.7.0 is the first one where the version of Node in the Docker
    image supports top-level await.

--- cloudflare-playwright.sh
+++ cloudflare-playwright.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$(readlink --canonicalize "$0")")"
-docker run --rm --volume "$PWD":/app:ro mcr.microsoft.com/playwright:v$(node --eval='console.log(require("playwright-core/package.json").version)') node /app $1
+docker run --rm --volume "$PWD":/app:ro mcr.microsoft.com/playwright:v$(node --eval='console.log(require("./node_modules/playwright-core/package.json").version)') node /app $1
