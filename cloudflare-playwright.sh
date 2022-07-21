e78e48786b44ae9b6a46da8148ff7be956604c39
Conduitry <git@chor.date>
2022-07-21 08:08:46 -0400

    unpin playwright-core version

--- cloudflare-playwright.sh
+++ cloudflare-playwright.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run --rm --volume "$(dirname "$(readlink --canonicalize "$0")")":/app:ro mcr.microsoft.com/playwright:v1.22.2 node /app $1
+cd "$(dirname "$(readlink --canonicalize "$0")")"
+docker run --rm --volume "$PWD":/app:ro mcr.microsoft.com/playwright:v$(node --eval='console.log(require("playwright-core/package.json").version)') node /app $1
