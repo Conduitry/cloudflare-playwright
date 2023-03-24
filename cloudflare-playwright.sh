5b1bbaaf23d12c37af36d7142555bf90022f6943
Conduitry <git@chor.date>
2023-03-23 21:55:18 -0400

    expose $DISPLAY and /tmp/.X11-unix to Docker container

--- cloudflare-playwright.sh
+++ cloudflare-playwright.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$(readlink --canonicalize "$0")")"
-docker run --rm --volume "$PWD":/app:ro mcr.microsoft.com/playwright:v$(node --eval='console.log(require("./node_modules/playwright-core/package.json").version)') node /app $1
+docker run --env DISPLAY --rm --volume "$PWD":/app:ro --volume /tmp/.X11-unix:/tmp/.X11-unix mcr.microsoft.com/playwright:v$(node --eval='console.log(require("./node_modules/playwright-core/package.json").version)') node /app $1
